1964596a762c7838cfb820ca0128401afb4fbd4a
clement <clement.cauliez@gmail.com>
2021-11-15 20:01:37 +0100

    added user, tools and test classes

---
+++ src/main/java/fr/oukilson/app/entity/Game.java
@@ -0,0 +1,4 @@
+package fr.oukilson.app.entity;
+
+public class Game {
+}
---
+++ src/main/java/fr/oukilson/app/entity/Token.java
@@ -0,0 +1,10 @@
+package fr.oukilson.app.entity;
+
+import java.awt.*;
+
+public class Token {
+
+    private int tokenId;
+    private String tokenName;
+    private Image tokenImage;
+}
