c52ab4877a206e6c9c7f733d56645c5948fad046
clement <clement.cauliez@gmail.com>
2021-12-02 21:21:01 +0100

    tested waiting list methods

--- src/Test/java/fr/oukilson/app/entity/EventTest.java
+++ src/Test/java/fr/oukilson/app/entity/EventTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
+
 public class EventTest {
 
-    // testing the player number setter (valid, min < 2, max < 2, min > max)
+    // testing the player number setter (valid(min & max), min < 2, max < 2, min > max)
     @Test
     @DisplayName("Testing the function to set the number of players, valid arguments")
     public void testSetPlayersNumberNoExceptionMin(){
@@ -47,10 +49,127 @@ public class EventTest {
                 event.setPlayersNumber(min, max));
     }
 
-    // testing setPlayersNumber function with min & max being the same
+    // testing setPlayersNumber function with min & max being the same (valid(min & max), minmax < 2)
     @Test
     @DisplayName("Testing the function to set the number of players if they're equal, valid arguments.")
-    public void testsetPlayersNumberIfEqualNoException(){
-        
+    public void testSetPlayersNumberIfEqualNoExceptionMin(){
+        Event event = new Event ("Event 1");
+        int minmax = 5;
+        event.setPlayersNumber(minmax);
+        Assertions.assertEquals(event.getMinPlayer(), minmax);
     }
+
+    @Test
+    @DisplayName("Testing the function to set the number of players if they're equal, valid arguments.")
+    public void testSetPlayersNumberIfEqualNoExceptionMax(){
+        Event event = new Event ("Event 1");
+        int minmax = 5;
+        event.setPlayersNumber(minmax);
+        Assertions.assertEquals(event.getMaxPlayer(), minmax);
+    }
+
+    @Test
+    @DisplayName("Testing the function to set the number of players if they're equal, invalid argument.")
+    public void testSetPlayersNumberIfEqualIllegalArgumentException(){
+        Event event = new Event("Event 1");
+        int minmax = 1;
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                event.setPlayersNumber(minmax));
+    }
+
+    // testing function to add a user to an event's waiting queue(valid, list > max)
+    @Test
+    @DisplayName("Testing the function to add a user to an event's waiting list, valid arguements : returns true")
+    public void testAddUserInEventWaitingQueueAssertTrue(){
+        long millis = System.currentTimeMillis();
+        Date creationDate = new Date((long) (millis * 0.98));
+        Date startingDate = new Date(millis);
+        Event event = new Event("Event 1", creationDate, startingDate, 5, 7,false);
+        User user = new User ("Jean");
+        Assertions.assertTrue(event.addUserInEventWaitingQueue(user));
+    }
+            // TODO waiting list can't be accessed so can't check if the user is put it it
+    @Test
+    @DisplayName("Testing the function to add a user to an event's waiting list, valid arguements : user in queue")
+    public void testAddUserInEventWaitingQueueAssertTrueUser(){
+        long millis = System.currentTimeMillis();
+        Date creationDate = new Date((long) (millis * 0.98));
+        Date startingDate = new Date(millis);
+        Event event = new Event("Event 1", creationDate, startingDate, 5, 7,false);
+        User user = new User ("Jean");
+        event.addUserInEventWaitingQueue(user);
+        Assertions.assertEquals(event.getWaitingUsers().get(user.getUUID()), user);
+    }
+
+    @Test
+    @DisplayName("Testing the function to add a user to an event's waiting list, invalid arguements(list full) : returns false")
+    public void testAddUserInEventWaitingQueueAssertFalse() {
+        long millis = System.currentTimeMillis();
+        Date creationDate = new Date((long) (millis * 0.98));
+        Date startingDate = new Date(millis);
+        Event event = new Event("Event 1", creationDate, startingDate, 2, 5, false);
+        User user = new User("Jean");
+        User user2 = new User("Jean");
+        User user3 = new User("Jean");
+        User user4 = new User("Jean");
+        User user5 = new User("Jean");
+        User user6 = new User("Jean");
+        event.addUserInEventWaitingQueue(user);
+        event.addUserInEventWaitingQueue(user2);
+        event.addUserInEventWaitingQueue(user3);
+        event.addUserInEventWaitingQueue(user4);
+        event.addUserInEventWaitingQueue(user5);
+        Assertions.assertFalse(event.addUserInEventWaitingQueue(user6));
+    }
+
+    // testing function to remove a user from an event's waiting queue(valid(user removed, user wasn't in queue =>null)
+    @Test
+    @DisplayName("Testing the function to remove a user from an event's waiting list, valid argument, removed")
+    public void testRemoveUserInEventWaitingQueue(){
+        long millis = System.currentTimeMillis();
+        Date creationDate = new Date((long) (millis * 0.98));
+        Date startingDate = new Date(millis);
+        Event event = new Event("Event 1", creationDate, startingDate, 2, 5, false);
+        User user = new User("Jean");
+        event.addUserInEventWaitingQueue(user);
+        Assertions.assertTrue(event.removeUserInEventWaitingQueue(user.getUUID()));
+        //Assertions.assertNull(event.getWaitingUsers().get(user.getUUID()));
+    }
+
+    @Test
+    @DisplayName("Testing the function to remove a user from an event's waiting list, invalid argument, null")
+    public void testRemoveUserInEventWaitingQueueNull() {
+        long millis = System.currentTimeMillis();
+        Date creationDate = new Date((long) (millis * 0.98));
+        Date startingDate = new Date(millis);
+        Event event = new Event("Event 1", creationDate, startingDate, 2, 5, false);
+        User user = new User("Jean");
+        Assertions.assertNull(event.removeUserInEventWaitingQueue(user.getUUID()));
+    }
+
+    // testing function to get a user from an event's waiting queue(valid(user gotten, user wasn't in queue =>null)
+    @Test
+    @DisplayName("Testing the function to get a user from an event's waiting list, valid argument, gotten")
+    public void testGetUserFromEventWaitingQueue(){
+        long millis = System.currentTimeMillis();
+        Date creationDate = new Date((long) (millis * 0.98));
+        Date startingDate = new Date(millis);
+        Event event = new Event("Event 1", creationDate, startingDate, 2, 5, false);
+        User user = new User("Jean");
+        event.addUserInEventWaitingQueue(user);
+        Assertions.assertEquals(event.getUserFromEventWaitingQueue(user.getUUID()), user);
+    }
+
+    @Test
+    @DisplayName("Testing the function to get a user from an event's waiting list, invalid argument, null")
+    public void testGetUserFromEventWaitingQueueNull() {
+        long millis = System.currentTimeMillis();
+        Date creationDate = new Date((long) (millis * 0.98));
+        Date startingDate = new Date(millis);
+        Event event = new Event("Event 1", creationDate, startingDate, 2, 5, false);
+        User user = new User("Jean");
+        Assertions.assertNull(event.getUserFromEventWaitingQueue(user.getUUID()));
+    }
+
+    
 }
--- src/main/java/fr/oukilson/app/App.java
+++ src/main/java/fr/oukilson/app/App.java
@@ -1,5 +1,7 @@
 package fr.oukilson.app;
 
+import java.time.LocalTime;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(LocalTime.now());
     }
 }
